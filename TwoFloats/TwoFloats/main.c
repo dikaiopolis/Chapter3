b73fdec31e22379f613abaafd50db1d738f6bec0
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-09-06 14:33:42 -0500

    finished exercise files

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Daniel Baldwin on 8/12/13.
+//  Copyright (c) 2013 Daniel Baldwin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i = 20;
+    int j = 25;
+    
+    int k = (i > j) ? 10 : 5;
+    
+    if (5 < j - k) {//first expression
+        printf("The first expression is true.");
+    return 0;
+}
+
+}
